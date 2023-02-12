bf27b110b6fd570154d22963c8cfd40cac81c69f
Rarova96 <125203745+Rarova96@users.noreply.github.com>
2023-02-12 17:21:19 +0100

    Add files via upload
    Comienzo del ejercicio

---
+++ CorteIrlandés.java
@@ -0,0 +1,43 @@
+/*La empresa El Corte Islandés nos ha encargado una aplicación para gestionar las tarjetas regalo. Como primer paso para implementar la aplicación, 
+    es necesario crear la clase principal. Implementa la clase TarjetaRegalo. Cuando se crea una nueva tarjeta, se le da un saldo y se asigna de forma automática un número 
+    de 5 dígitos. Si se intenta gastar más dinero del que tiene la tarjeta, se debe mostrar un mensaje de error. Dos tarjetas regalo se pueden fusionar creando una nueva 
+    tarjeta con la suma del saldo que tenga cada una y un nuevo número aleatorio de 5 cifras. Al fusionar dos tarjetas en una, las dos tarjetas originales se quedarían con 0 € 
+    de saldo.
+
+    Programa principal:
+
+    TarjetaRegalo t1 = new TarjetaRegalo(100); 
+
+    TarjetaRegalo t2 = new TarjetaRegalo(120); 
+
+    t1.gasta(45.90);
+    t2.gasta(5);
+    t2.gasta(200);
+    t1.gasta(3.55);
+    System.out.println(t1.comprobarSaldo());
+    System.out.println(t2.comprobarSaldo());
+    TarjetaRegalo t3 = t1.fusionaCon(t2);
+    System.out.println(t1.comprobarSaldo());
+    System.out.println(t2.comprobarSaldo());
+    System.out.println(t3.comprobarSaldo());
+    Salida:
+
+    No tiene suficiente saldo para gastar 200,00€
+    50.550000000000004
+    115.0
+    0.0
+    0.0
+    165.55
+    */
+
+
+
+
+
+public class CorteIrlandés{
+
+
+
+
+
+}
